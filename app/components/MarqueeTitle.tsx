1d1bf0016e93afc723c58f48fec02fd9542d73e7
LeandroDukievicz <ldukie@icloud.com>
2026-03-09 22:20:12 -0300

    feat: update favicon, add marquee title animation and foto-1 asset
    - Replaced favicon.ico with new converted image (512x512 png -> ico)
    - Created MarqueeTitle component with scrolling title animation in browser tab
    - Added MarqueeTitle to RootLayout for global use
    - Updated metadata title and description in layout.tsx
    - Added foto-1.webp to public/images

---
+++ app/components/MarqueeTitle.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+
+const TITLE = "Bem vindo ao meu portifólio | Leandro Dukievicz | Dev Front End \u00a0\u00a0\u00a0";
+
+export default function MarqueeTitle() {
+  useEffect(() => {
+    let index = 0;
+
+    const interval = setInterval(() => {
+      document.title = TITLE.slice(index) + TITLE.slice(0, index);
+      index = (index + 1) % TITLE.length;
+    }, 150);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return null;
+}
